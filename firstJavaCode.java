5edd9bdc4813eb25742b4d32647b642dc8abaef3
Dept. of CST, HEI <headway.cst@gmail.com>
2025-10-30 12:00:07 +0600

    Add files via upload

---
+++ firstJavaCode.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class firstJavaCode {
+
+    public static void main(String[] args) {
+
+        // Arithmatic Operator
+        // int number1 = 13;
+        // int number2 = 5;
+        // int sum = number1 + number2;
+        // int sub = number1 - number2;
+        // int mul = number1 * number2;
+        // float div = number1 / number2;
+        // int mod = number1 % number2;
+        // System.out.println(sum);
+        // System.out.println(sub);
+        // System.out.println(mul);
+        // System.out.println(div);
+        // System.out.println(mod);
+        // Assignment Operator
+        // int x = 10;
+        // x += 2;
+        // x -= 4;
+        // x *= 2;
+        // x /= 4;
+        // x %= 2;
+        // System.out.println(x);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the number b: ");
+        int b = sc.nextInt();
+
+        System.out.println(a + b);  
+        
+    }
+}
